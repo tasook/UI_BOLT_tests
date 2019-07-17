354be046467e4dc3c8f00a3df1a3674f8f3f8e68
Tasos <tasosb@gmail.com>
2019-07-17 16:13:16 +0100

    UI transitions, controlled by BOLT

---
+++ Assets/ExampleClass.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExampleClass : MonoBehaviour
+{
+    void Start()
+    {
+        WebCamTexture webcamTexture = new WebCamTexture();
+        Renderer renderer = GetComponent<Renderer>();
+        renderer.material.mainTexture = webcamTexture;
+        webcamTexture.Play();
+    }
+}
